3b12a6409222cd10ef3279c60d0cb3dff12a41ca
alex-santelli <alex.santelli.junior@escola.pr.gov.br>
2024-10-01 16:40:21 -0300

    Add files via upload

---
+++ assets/pergunta.js
@@ -0,0 +1,22 @@
+criarCartao(
+    'programação',
+    'O que Python?',
+    'o Python é uma linguagem de programação',
+
+)
+
+criarCartao(
+
+
+'Georgrafia',
+'Qual é a capital da frança?',
+'A capital da França é Paris',
+)
+
+criarCartao(
+
+'programação',
+'O que é uma função?',
+'Uma função é um bloco de código que executa alguma tarefa',
+
+)
